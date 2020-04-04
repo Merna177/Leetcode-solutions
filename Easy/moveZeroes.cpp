5c45b9d392bfe65a6dbab0bbc660c75497a5ee4b
Merna177 <37479454+Merna177@users.noreply.github.com>
2020-04-04 16:07:23 +0200

    solve-->https://leetcode.com/problems/move-zeroes/

---
+++ Easy/moveZeroes.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int pointer=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                swap(nums[pointer++],nums[i]);
+            }
+        }
+    }
+};
