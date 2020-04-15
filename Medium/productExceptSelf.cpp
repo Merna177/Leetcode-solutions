0e96563f7c236df008b418c5bfc2f377059e7f5f
Merna177 <37479454+Merna177@users.noreply.github.com>
2020-04-15 13:20:42 +0200

    solve-->https://leetcode.com/problems/product-of-array-except-self/

---
+++ Medium/productExceptSelf.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> ans(nums.size(),1);
+        ///accumlative (left product)
+        for(int i=1;i<nums.size();i++){
+            ans[i]=ans[i-1]*nums[i-1];
+        }
+        int right=nums[nums.size()-1];
+        for(int i=nums.size()-2;i>=0;i--){
+            ans[i]*=right;
+            right*=nums[i];
+        }
+        return ans;
+        
+    }
+};
